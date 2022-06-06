4c17d7034a9be0e63daf84a619ed3c43cc530314
[Elena Bellart Urgell] <[ebellart@uoc.edu]>
2022-06-06 15:11:07 +0200

    First commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+
+let fundacio = document.getElementById('fundacio');
+fundacio.onmouseover = function () {
+  fundacio.classList.add("hover");
+};
+
+fundacio.onmouseout = function () {
+  fundacio.classList.remove("hover");
+}
+
+let robots = document.getElementById('robots');
+robots.onmouseover = function () {
+  robots.classList.add("hover");
+};
+
+robots.onmouseout = function () {
+  robots.classList.remove("hover");
+}
+
+
